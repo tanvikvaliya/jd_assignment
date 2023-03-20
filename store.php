316b45e4dc4d9c1ed28250b52892767a3aeb14cc
tanvik valiya <tanvikvaliya@gmail.com>
2023-03-20 11:59:32 +0530

    code optimize

--- store.php
+++ store.php
@@ -3,17 +3,14 @@
     if (!$link) {
         die("Connection failed: " . mysqli_connect_error());
     }
+
     $name = $_POST['name'];
-
     $address = $_POST['address'];
-
     $email = $_POST['email'];
-
     $mobile = $_POST['mobile'];
-
     $city = $_POST['city'];
-
     $country = $_POST['country'];
+    
     if(isset($_POST["user_id"]) && !empty(trim($_POST["user_id"]))){
         $id = trim($_POST["user_id"]);
         $sql = "UPDATE user_master SET name='$name',
@@ -28,12 +25,8 @@
     }
     if(mysqli_query($link, $sql)){
         header("location: index.php");
- 
-           
     }else{
-
-        echo "Error:". $sql . "<br>". $link->error;
-
+        echo "Something went wrong! please check the code";
     } 
     $link->close(); 
 ?>
